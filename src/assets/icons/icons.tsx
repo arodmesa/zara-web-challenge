2446483e02c371a4fffdb43399cb0c6d06e3cb4e
Alex <arodmesa@gmail.com>
2024-03-06 12:11:00 +0100

    fixed console warnings

--- src/assets/icons/icons.tsx
+++ src/assets/icons/icons.tsx
@@ -8,8 +8,8 @@ export function HeartIcon({ className }: IconsProps) {
       className={className}
     >
       <path
-        fill-rule="evenodd"
-        clip-rule="evenodd"
+        fillRule="evenodd"
+        clipRule="evenodd"
         d="M6.5 2.35108L3.5 0.530273L0.5 2.35108V6.25282L6.5 11.3684L12.5 6.25282V2.35108L9.5 0.530273L6.5 2.35108Z"
         fill="currentColor"
       />
